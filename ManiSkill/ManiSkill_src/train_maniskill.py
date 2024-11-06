fixed bug with semaphores leakage in maniskill env

ManiSkill/ManiSkill_src/train_maniskill.py
@@ -6,7 +6,7 @@ import comet_ml
 import argparse
 import yaml
 # from torch import multiprocessing
-import torch.multiprocessing as mp
+# import torch.multiprocessing as mp
 from torch.utils.data import DataLoader
 # try:
 #     multiprocessing.set_start_method('spawn')
@@ -24,8 +24,8 @@ from VizDoom.VizDoom_src.train import train
 from TMaze_new.TMaze_new_src.utils import set_seed, get_intro_maniskill
 from ManiSkill.ManiSkill_src.utils import ManiSkillIterDataset
 
-import warnings
-warnings.filterwarnings("ignore", category=UserWarning, message="resource_tracker:.*")
+# import warnings
+# warnings.filterwarnings("ignore", category=UserWarning, message="resource_tracker:.*")
 
 
 # os.environ["MKL_NUM_THREADS"] = "1" 
@@ -61,6 +61,9 @@ def create_args():
     return parser
 
 if __name__ == '__main__':
+    import multiprocessing as mp
+    mp.set_start_method("spawn")
+
     
     get_intro_maniskill()
     

VizDoom/VizDoom_src/train/trainer.py
@@ -25,10 +25,10 @@ from RATE_GTrXL import mem_transformer_v2_GTrXL
 # from MinigridMemory.MinigridMemory_src.inference.val_minigridmemory import get_returns_MinigridMemory
 
 
+import threading
 
-
-import warnings
-warnings.filterwarnings('ignore')
+# import warnings
+# warnings.filterwarnings('ignore')
 import torch.nn as nn
 
 reds = [2, 3, 6, 8, 9, 10, 11, 14, 15, 16, 17, 18, 20, 21, 25, 26, 27, 28, 29, 31, 38, 40, 41, 42, 45,
@@ -56,6 +56,23 @@ def train(ckpt_path, config, train_dataloader, mean, std, max_segments, experime
         from ManiSkill.ManiSkill_src.inference.val_maniskill import get_returns_ManiSkill
         from collections import defaultdict
         tokens_cnt = tokens_cnt_step = 2_000_000
+
+        import gymnasium as gym
+        import mani_skill.envs
+        from mani_skill.utils import gym_utils
+        from mani_skill.utils.wrappers.flatten import FlattenActionSpaceWrapper, FlattenRGBDObservationWrapper
+        from mani_skill.utils.wrappers.record import RecordEpisode
+        from mani_skill.vector.wrappers.gymnasium import ManiSkillVectorEnv
+
+        eval_output_dir = f"ManiSkill/val_videos/{config['model_mode']}/{config['text_description']}"
+        env_kwargs = dict(obs_mode="rgb", control_mode="pd_joint_delta_pos", render_mode="all", sim_backend="gpu")
+
+        env = gym.make("PushCube-v1", num_envs=1, **env_kwargs) 
+        env = FlattenRGBDObservationWrapper(env, rgb=True, depth=False, state=True)
+        if isinstance(env.action_space, gym.spaces.Dict):
+            env = FlattenActionSpaceWrapper(env) 
+        env = RecordEpisode(env, output_dir=eval_output_dir, save_trajectory=False, trajectory_name=f"rate_val", max_steps_per_video=50, video_fps=30)
+        env = ManiSkillVectorEnv(env, 1, ignore_terminations=True, record_metrics=True)
     else:
         raise NotImplementedError
 
@@ -72,6 +89,7 @@ def train(ckpt_path, config, train_dataloader, mean, std, max_segments, experime
     model = mem_transformer_v2_GTrXL.MemTransformerLM(**config["model_config"])
 
     print(config)
+    print(f"device: {device}")
     
     torch.nn.init.xavier_uniform_(model.r_w_bias);
     torch.nn.init.xavier_uniform_(model.r_r_bias);
@@ -472,12 +490,13 @@ def train(ckpt_path, config, train_dataloader, mean, std, max_segments, experime
                                             f"FINAL_ReturnsMean_r_{ret}_l_{LENGTH}":    returns_mean}, step=it_counter)
         
         elif config["model_config"]["mode"] == 'maniskill-pushcube':
-            if tokens > tokens_cnt:
-                tokens_cnt += tokens_cnt_step
-                if tokens_cnt > 50_000_000:
-                    tokens_cnt_step = 4_000_000
-                elif tokens_cnt > 25_000_000:
-                    tokens_cnt_step = 4_000_000
+            if  ((epoch + 1) % int(config["training_config"]["ckpt_epoch"])) == 0 or epoch == config["training_config"]["epochs"] - 1 or (epoch + 1) == 1:
+            # if tokens > tokens_cnt:
+            #     tokens_cnt += tokens_cnt_step
+            #     if tokens_cnt > 50_000_000:
+            #         tokens_cnt_step = 4_000_000
+            #     elif tokens_cnt > 25_000_000:
+            #         tokens_cnt_step = 4_000_000
                 if config["model_config"]["mode"] == 'maniskill-pushcube' and config["training_config"]["online_inference"]:
                     model.eval()
                     logged_video_seeds = 0
@@ -485,8 +504,10 @@ def train(ckpt_path, config, train_dataloader, mean, std, max_segments, experime
                     medium_video_logged = False
                     good_video_logged = False
                     with torch.no_grad():
-                        seeds = np.arange(0, 100).tolist()[::5]
+                        # seeds = np.arange(0, 100).tolist()[::5]
                         # seeds = [123, 231, 321, 777, 888]
+                        seeds = [16, 41, 64, 73, 26, # hard seeds
+                                 0, 93, 9, 15, 19] # easy seeds
                         total_rew_mm = 0
                         cnt = 1
                         for ret in [config["online_inference_config"]["desired_return_1"]]:
@@ -502,7 +523,7 @@ def train(ckpt_path, config, train_dataloader, mean, std, max_segments, experime
 
                             for i in range(len(seeds)):
                                 try:
-                                    episode_return, _, t, _, _, _, _, eval_metrics = get_returns_ManiSkill(model=model, ret=ret, seed=seeds[i], 
+                                    episode_return, _, t, _, _, _, _, eval_metrics = get_returns_ManiSkill(env=env, model=model, ret=ret, seed=seeds[i], 
                                                                                             episode_timeout=episode_timeout, 
                                                                                             context_length=config["training_config"]["context_length"], 
                                                                                             device=device, act_dim=config["model_config"]["ACTION_DIM"], 
@@ -512,10 +533,11 @@ def train(ckpt_path, config, train_dataloader, mean, std, max_segments, experime
                                                                                             use_argmax=config["online_inference_config"]["use_argmax"],
                                                                                             create_video=False)
 
+                                    model.to(device)
                                     returns.append(episode_return)
                                     ts.append(t)
 
-                                    time.sleep(0.1)
+                                    # time.sleep(0.1)
                                     torch.cuda.empty_cache()
                                     
 
@@ -568,4 +590,6 @@ def train(ckpt_path, config, train_dataloader, mean, std, max_segments, experime
                 elif wcomet:
                     experiment.log_metrics({"checkpoint_step": tokens.item()}, step=it_counter)
                 torch.save(model.state_dict(), ckpt_path + str(tokens.item()) + '_KTD.pth')
+    if env is not None:
+        env.close()
     return model