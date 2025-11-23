c0cf24bc6db3b0728d1af584ab45c7ecf006df22
Egor Cherepanov <cherepanovegor2018@gmail.com>
2025-11-23 11:55:57 +0000

    added probing

--- Elastic-DT/scripts/run_vizdoom_train.sh
+++ Elastic-DT/scripts/run_vizdoom_train.sh
@@ -11,48 +11,21 @@ echo "Dataset: $DATASET_DIR"
 echo "Seed: $SEED"
 
 # * Initial parameters
-# python Elastic-DT/scripts/train_edt_vizdoom.py \
-#     --dataset_dir "$DATASET_DIR" \
-#     --context_len 50 \
-#     --n_blocks 4 \
-#     --embed_dim 128 \
-#     --n_heads 4 \
-#     --dropout_p 0.1 \
-#     --act_dim 5 \
-#     --batch_size 128 \
-#     --lr 1e-4 \
-#     --wt_decay 1e-4 \
-#     --warmup_steps 10000 \
-#     --max_train_iters 500 \
-#     --num_updates_per_iter 100 \
-#     --model_save_iters 50 \
-#     --rtg_scale 1000 \
-#     --num_bin 60 \
-#     --expectile 0.99 \
-#     --exp_loss_weight 0.5 \
-#     --ce_weight 0.001 \
-#     --seed "$SEED" \
-#     --device cuda \
-#     --chk_pt_dir checkpoints/vizdoom/ \
-#     --use_wandb \
-#     --project_name EDT-ViZDoom
-
-# * Small parameters to run test
 python Elastic-DT/scripts/train_edt_vizdoom.py \
     --dataset_dir "$DATASET_DIR" \
-    --context_len 150 \
-    --n_blocks 2 \
-    --embed_dim 32 \
-    --n_heads 2 \
+    --context_len 50 \
+    --n_blocks 6 \
+    --embed_dim 128 \
+    --n_heads 8 \
     --dropout_p 0.1 \
     --act_dim 5 \
-    --batch_size 16 \
-    --lr 1e-4 \
+    --batch_size 128 \
+    --lr 3e-4 \
     --wt_decay 1e-4 \
     --warmup_steps 10000 \
     --max_train_iters 500 \
     --num_updates_per_iter 100 \
-    --model_save_iters 2 \
+    --model_save_iters 50 \
     --rtg_scale 1 \
     --num_bin 60 \
     --expectile 0.99 \
@@ -63,6 +36,33 @@ python Elastic-DT/scripts/train_edt_vizdoom.py \
     --chk_pt_dir checkpoints/vizdoom/ \
     --use_wandb \
     --project_name EDT-ViZDoom
+
+# * Small parameters to run test
+# python Elastic-DT/scripts/train_edt_vizdoom.py \
+#     --dataset_dir "$DATASET_DIR" \
+#     --context_len 150 \
+#     --n_blocks 2 \
+#     --embed_dim 32 \
+#     --n_heads 2 \
+#     --dropout_p 0.1 \
+#     --act_dim 5 \
+#     --batch_size 16 \
+#     --lr 1e-4 \
+#     --wt_decay 1e-4 \
+#     --warmup_steps 10000 \
+#     --max_train_iters 500 \
+#     --num_updates_per_iter 100 \
+#     --model_save_iters 2 \
+#     --rtg_scale 1 \
+#     --num_bin 60 \
+#     --expectile 0.99 \
+#     --exp_loss_weight 0.5 \
+#     --ce_weight 0.001 \
+#     --seed "$SEED" \
+#     --device cuda \
+#     --chk_pt_dir checkpoints/vizdoom/ \
+#     --use_wandb \
+#     --project_name EDT-ViZDoom
 
 echo "Training complete!"
 
